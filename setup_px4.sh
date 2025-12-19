2a402e052e3c1bab3b7b340e4aa769d4dd6aaac0
Walker Yee <walkeryee26@gmail.com>
2025-12-19 17:33:04 -0500

    added scan topic and make small change to setup_px4.sh

--- setup_px4.sh
+++ setup_px4.sh
@@ -16,13 +16,19 @@ sudo apt-get update
 sudo apt-get install -y git python3-pip python3-colcon-common-extensions
 
 # 2. Clone PX4-Autopilot
+if [ -d "PX4-Autopilot" ] && [ -z "$(ls -A PX4-Autopilot)" ]; then
+    echo "Deleting empty PX4-Autopilot Directory"
+    rmdir PX4-Autopilot
+fi
+
 if [ ! -d "PX4-Autopilot" ]; then
     echo "Cloning PX4-Autopilot (v1.14)..."
     git clone https://github.com/PX4/PX4-Autopilot.git --recursive -b release/1.14
 else
     echo "PX4-Autopilot already exists. Skipping clone."
 fi
-
+ 
+    
 # 3. Install PX4 dependencies
 echo "Installing PX4 dependencies (this may ask for sudo password)..."
 cd PX4-Autopilot
@@ -34,6 +40,10 @@ make px4_sitl_default
 
 # 5. Install MicroXRCEAgent (ROS 2 Bridge)
 cd ..
+if [ -d "Micro-XRCE-DDS-Agent" ] && [ -z "$(ls -A Micro-XRCE-DDS-Agent)" ]; then
+    echo "Deleting empty Micro-XRCE-DDS-Agent Directory"
+    rmdir Micro-XRCE-DDS-Agent
+fi
 if [ ! -d "Micro-XRCE-DDS-Agent" ]; then
     echo "Cloning Micro-XRCE-DDS-Agent..."
     git clone https://github.com/eProsima/Micro-XRCE-DDS-Agent.git
